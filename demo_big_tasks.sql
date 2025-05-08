93c1a2988abc45d1510205e63d7692e49f87ea28
whiteprincewithobsession <yarik_02022005@mail.ru>
2025-05-08 20:33:23 +0300

    huge fix

--- demo_big_tasks.sql
+++ demo_big_tasks.sql
@@ -11,47 +11,61 @@ ORDER BY passenger_name ASC;
 
 -- TASK 3
 
-SELECT tickets.passenger_id, MAX(ticket_flights.amount) AS max_price FROM tickets
+SELECT tickets.passenger_name, MAX(ticket_flights.amount) AS max_price FROM tickets
 JOIN ticket_flights ON tickets.ticket_no = ticket_flights.ticket_no 
-GROUP BY tickets.passenger_id;
+GROUP BY tickets.passenger_name;
 
 -- TASK 4
-
-SELECT t.passenger_id, t.passenger_name FROM tickets t
+-- выведите всех пассажиров которые НЕ бронировали билеты эконом класса в январе 2016 года
+SELECT t.passenger_name FROM tickets t
 WHERE NOT EXISTS (
     SELECT 1 FROM ticket_flights tf
     JOIN flights ON tf.flight_id = flights.flight_id
     JOIN seats ON flights.aircraft_code = seats.aircraft_code
-    WHERE  tf.ticket_no = t.ticket_no AND (flights.status = 'Arrived' OR flights.status = 'Departed') AND seats.fare_conditions = 'Economy' 
-        AND ((flights.actual_arrival >= '2016-01-01' AND flights.actual_arrival < '2016-02-01')
-            OR (flights.actual_departure >= '2016-01-01' AND flights.actual_departure < '2016-02-01')
-        )
+    WHERE  tf.ticket_no = t.ticket_no AND seats.fare_conditions = 'Economy' 
+        AND t.book_ref IN (
+          SELECT book_ref FROM bookings 
+          WHERE book_date >= '2016-01-01' AND book_date < '2016-02-01'
+      )
 );
+
+SELECT DISTINCT t.passenger_id, t.passenger_name
+FROM bookings.tickets t
+LEFT JOIN (
+    SELECT tf.ticket_no
+    FROM bookings.ticket_flights tf
+    JOIN bookings.flights f ON tf.flight_id = f.flight_id
+    WHERE tf.fare_conditions = 'Economy'
+    AND f.scheduled_departure >= '2016-01-01' 
+    AND f.scheduled_departure < '2016-02-01'
+) eco_tickets ON t.ticket_no = eco_tickets.ticket_no
+WHERE eco_tickets.ticket_no IS NULL;
 
 -- TASK 5
 
 --  St. Petersburg
 -- Moscow
 
-SELECT COUNT(*) FILTER 
-    (WHERE departure_airport IN 
-        (SELECT airport_code FROM airports 
-            WHERE city = 'Moscow'
-        )
-    ) AS moscow_flights,
-    COUNT(*) FILTER 
-    (WHERE departure_airport IN 
-        (SELECT airport_code FROM airports 
-            WHERE city = 'St. Petersburg'
-        )
-    ) AS spb_flights
-FROM flights;
+(
+    SELECT airports.city AS departure_city, COUNT(*) AS flights_count 
+    FROM flights 
+    JOIN airports ON flights.departure_airport = airports.airport_code
+    WHERE airports.city = 'Moscow'
+    GROUP BY airports.city
+)
+UNION ALL
+(
+    SELECT airports.city AS departure_city, COUNT(*) AS flights_count 
+    FROM flights JOIN airports ON flights.departure_airport = airports.airport_code
+    WHERE  airports.city = 'St. Petersburg'
+    GROUP BY airports.city
+);
 
 -- TASK 6
 
 SELECT flight_id AS actual_departure,
-    (actual_arrival - LAG(actual_arrival, 1) OVER (ORDER BY flight_id)) AS prev_dep_interval,
-    (LEAD(actual_arrival, 1) OVER (ORDER BY flight_id) - actual_arrival) AS next_dep_interval 
+    (actual_departure - LAG(actual_departure, 1) OVER (ORDER BY flight_id)) AS prev_dep_interval,
+    (LEAD(actual_departure, 1) OVER (ORDER BY flight_id) - actual_departure) AS next_dep_interval 
 FROM flights
 WHERE status = 'Arrived';
 
@@ -60,18 +74,14 @@ WHERE status = 'Arrived';
 WITH consecutive_bookings AS (
     SELECT t.passenger_id, t.passenger_name,
     b.book_ref, b.book_date, b.total_amount,
-    RANK() OVER (ORDER BY b.total_amount DESC) as r_amount,
-    LAG(b.total_amount, 1) OVER (ORDER BY b.book_date) as prev1,
-    LAG(b.total_amount, 2) OVER (ORDER BY b.book_date) as prev2,
-    LAG(b.total_amount, 3) OVER (ORDER BY b.book_date) as prev3,
-    LAG(b.total_amount, 4) OVER (ORDER BY b.book_date) as prev4
+    SUM(b.total_amount) OVER (ORDER BY b.book_date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) as total_amount_5
     FROM tickets t
     JOIN bookings b ON t.book_ref = b.book_ref
 )
-SELECT passenger_id, passenger_name, total_amount,
-    (COALESCE(prev1, 0) + COALESCE(prev2, 0) + COALESCE(prev3, 0) + COALESCE(prev4, 0) + total_amount) as consecutive_sum
+SELECT passenger_id, passenger_name, total_amount, book_date, 
+    total_amount_5 as consecutive_sum
 FROM consecutive_bookings
-WHERE r_amount = 1
+ORDER BY consecutive_sum DESC
 LIMIT 1;
 
 -- TASK 8
